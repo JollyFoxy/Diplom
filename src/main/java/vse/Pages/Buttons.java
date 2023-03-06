cdf2d7f64b054c7fe126e9f68e9d649064040c44
pavel <pavel.mizirev@gmail.com>
2023-03-06 16:57:31 +0300

    Диплом походу не всё...

--- src/main/java/vse/Pages/Buttons.java
+++ src/main/java/vse/Pages/Buttons.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class Buttons  extends BasePage{
-    public final Transition transition=new Transition("vse/Elements","Buttons");
+    public final Transition transition=new Transition("Elements","Buttons");
     public final Button doubleClickBtn = new Button("doubleClickBtn");
     public final SelenideElement clickBtn = $(By.cssSelector("div[class$='col-md-6']> div:nth-child(2) > div:nth-child(3)> button"));
     //    ("div.row > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > button"))
